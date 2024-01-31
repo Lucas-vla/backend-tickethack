3da3807218662708f9d512cccefe256b2d627567
Lucas <lucasviola.pro@hotmail.com>
2024-01-31 14:50:27 +0100

    test

--- routes/trajets.js
+++ routes/trajets.js
@@ -5,7 +5,7 @@ var router = express.Router()
 const Trajet = require("../models/trajets") // Require le modèle trajets
 
 router.get('/:departure/:arrival/:date', (req, res) => {
-    Trajet.find({departure :{$regex : new RegExp(req.params.departure, 'i')}, arrival :{$regex : new RegExp(req.params.arrival, 'i') }, date: req.params.date}).then(data => {
+    Trajet.find({departure :{$regex : new RegExp(req.params.departure, 'i')}, arrival :{$regex : new RegExp(req.params.arrival, 'i') }}).then(data => {
         if(data.length !==0) {
             res.json({result: true, searchedTrip: data})
         } else {
